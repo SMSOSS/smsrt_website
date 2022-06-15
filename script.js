c76bdcc111aba123e90061a3dfa33c8aa119fae4
kotnid <tkt0506tkt@gmail.com>
2022-06-15 15:28:55 +0800

    smsrt_website : Add clock in home page
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ var s = skrollr.init([
     smoothScrolling=true
     ]
 );
+
+function currentTime(){
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let wd = date.getDay();
+
+    const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+    hh = (hh<10) ? "0"+hh : hh;
+    mm = (mm<10) ? "0"+mm : mm;
+
+    document.getElementById("hour").innerText = hh;
+    document.getElementById("min").innerText = mm;
+    document.getElementById("weekday").innerText = weekday[wd];
+
+    let t = setTimeout(function(){currentTime()}, 1000);
+}
+
+currentTime();
